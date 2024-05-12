f4bd218b48323039d4e3f94ed7e9a6d22e3fde6d
abdullah Al Omar <abdullahalomar048@gmail.com>
2024-05-13 00:57:22 +0600

    skill section customized

--- src/components/UI/Skill/Skill.tsx
+++ src/components/UI/Skill/Skill.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   Box,
   Container,
@@ -17,6 +18,18 @@ import mongo from "@/assets/img/skills/mongodb.png";
 import sql from "@/assets/img/skills/MySQL.png";
 import Image from "next/image";
 
+import Stack from "@mui/material/Stack";
+import Paper from "@mui/material/Paper";
+import { styled } from "@mui/material/styles";
+
+const DemoPaper = styled(Paper)(({ theme }) => ({
+  width: 120,
+  height: 120,
+  padding: theme.spacing(2),
+  ...theme.typography.body2,
+  textAlign: "center",
+}));
+
 const Skill = () => {
   return (
     <Box>
@@ -24,8 +37,45 @@ const Skill = () => {
         <Typography sx={{ fontSize: 40, fontWeight: 600 }}>
           My Skills
         </Typography>
-        <Grid container spacing={4} columns={20} mt={2}>
-          <Grid item xs={10} sm={10} md={4}>
+        <Stack
+          mt={2}
+          spacing={{ xs: 1, sm: 2 }}
+          marginLeft={{ xs: 5, sm: 5, md: 0, lg: 0 }}
+          direction="row"
+          useFlexGap
+          flexWrap="wrap"
+          divider={<Divider orientation="vertical" flexItem />}
+        >
+          <DemoPaper variant="outlined">
+            <Image src={bootstrap} height={70} width={90} alt="bootstrap" />
+          </DemoPaper>
+          <DemoPaper variant="outlined">
+            <Image src={tailwind} height={70} width={90} alt="bootstrap" />
+          </DemoPaper>
+          <DemoPaper variant="outlined">
+            <Image src={mui} height={70} width={90} alt="bootstrap" />
+          </DemoPaper>
+          <DemoPaper variant="outlined">
+            <Image src={js} height={70} width={90} alt="bootstrap" />
+          </DemoPaper>
+          <DemoPaper variant="outlined">
+            <Image src={react} height={70} width={90} alt="bootstrap" />
+          </DemoPaper>
+          <DemoPaper variant="outlined">
+            <Image src={next} height={70} width={90} alt="bootstrap" />
+          </DemoPaper>
+          <DemoPaper variant="outlined">
+            <Image src={node} height={70} width={90} alt="bootstrap" />
+          </DemoPaper>
+          <DemoPaper variant="outlined">
+            <Image src={mongo} height={4} width={40} alt="bootstrap" />
+          </DemoPaper>
+          <DemoPaper variant="outlined">
+            <Image src={sql} height={70} width={90} alt="bootstrap" />
+          </DemoPaper>
+        </Stack>
+        {/* <Grid container spacing={4} columns={20} mt={2}>
+          <Grid item xs={10} sm={10} md={4} sx={{ paddingX: 20 }}>
             <Tooltip title="Bootstrap" disableInteractive>
               <Image src={bootstrap} height={70} width={70} alt="bootstrap" />
             </Tooltip>
@@ -71,7 +121,7 @@ const Skill = () => {
               <Image src={sql} height={70} width={70} alt="bootstrap" />
             </Tooltip>
           </Grid>
-        </Grid>
+        </Grid> */}
       </Container>
     </Box>
   );
